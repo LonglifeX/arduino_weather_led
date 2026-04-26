59beba6909c3e1ce293f6ffde6e0c644c5bac535
Daniel Rabenau <danie@rabenau.org>
2026-04-26 09:51:36 +0200

    created platformio project structure

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <Arduino.h>
+#include <WiFiS3.h>
+#include <arduino_secrets.h>
+
+void setup() {
+    // write your initialization code here
+}
+
+void loop() {
+    // write your code here
+}
